e46813e385c257e6cb734e9cdffcd55e6ab9c294
zaru <sakuraba@basicinc.jp>
2015-10-27 18:33:47 +0900

    add zabbix recipe

--- lib/itamae/plugin/recipe/zabbix.rb
+++ lib/itamae/plugin/recipe/zabbix.rb
@@ -1,11 +1,17 @@
 require "itamae/plugin/recipe/zabbix/version"
 
-module Itamae
-  module Plugin
-    module Recipe
-      module Zabbix
-        # Your code goes here...
-      end
-    end
-  end
+zabbix_version = 2.2 # or 1.8 ~ 2.4
+zabbix_version = node[:zabbix][:version] if node[:zabbix][:version]
+
+platform = "rhel" # or debian, ubuntu
+platform = node[:distribution][:version] if node[:distribution][:version]
+
+platform_version = "6"
+platform_version = node[:platform_version] if node[:platform_version]
+
+cpu = "x86_64" # or i386
+cpu = node[:distribution][:cpu] if node[:distribution][:cpu]
+
+package "http://repo.zabbix.com/zabbix/#{zabbix_version}/#{platform}/#{platform_version}/#{cpu}/zabbix-release-#{zabbix_version}-1.el#{platform_version}.noarch.rpm" do
+  not_if "rpm -q zabbix-release"
 end
---
+++ lib/itamae/plugin/recipe/zabbix/agent.rb
@@ -0,0 +1,23 @@
+%w( zabbix-agent ).each do |pkg|
+  package pkg
+end
+
+template "/etc/zabbix/zabbix_agentd.conf" do
+  owner "root"
+  group "root"
+  source "./templates/etc/zabbix/zabbix_agentd.conf.erb"
+end
+
+template "/etc/zabbix/zabbix_agentd.d/userparameter_netstat.conf" do
+  owner "root"
+  group "root"
+  source "./templates/etc/zabbix/zabbix_agentd.d/userparameter_netstat.conf.erb"
+end
+
+service "zabbix-agent" do
+  subscribes :restart, "template[/etc/zabbix/zabbix_agentd.conf]"
+end
+
+service 'zabbix-agent' do
+  action [:enable, :start]
+end
