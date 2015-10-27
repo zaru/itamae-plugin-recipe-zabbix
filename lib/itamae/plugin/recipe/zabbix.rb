0a462302a6b7a1b7824939f91b8f036e205e378f
zaru <sakuraba@basicinc.jp>
2015-10-27 18:46:22 +0900

    fixed node key

--- lib/itamae/plugin/recipe/zabbix.rb
+++ lib/itamae/plugin/recipe/zabbix.rb
@@ -4,13 +4,13 @@ zabbix_version = 2.2 # or 1.8 ~ 2.4
 zabbix_version = node[:zabbix][:version] if node[:zabbix][:version]
 
 platform = "rhel" # or debian, ubuntu
-platform = node[:distribution][:version] if node[:distribution][:version]
+platform = node[:zabbix][:platform] if node[:zabbix][:platform]
 
 platform_version = "6"
-platform_version = node[:platform_version] if node[:platform_version]
+platform_version = node[:zabbix][:platform_version] if node[:zabbix][:platform_version]
 
 cpu = "x86_64" # or i386
-cpu = node[:distribution][:cpu] if node[:distribution][:cpu]
+cpu = node[:zabbix][:platform_cpu] if node[:zabbix][:platform_cpu]
 
 package "http://repo.zabbix.com/zabbix/#{zabbix_version}/#{platform}/#{platform_version}/#{cpu}/zabbix-release-#{zabbix_version}-1.el#{platform_version}.noarch.rpm" do
   not_if "rpm -q zabbix-release"
